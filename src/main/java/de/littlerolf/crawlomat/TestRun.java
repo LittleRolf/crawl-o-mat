3525be0be5e96f61a9513773571438c60118e2e4
Ole Wehrmeyer <olewehrmeyer@web.de>
2013-03-13 19:02:12 +0100

    Simpler Upload test

---
+++ src/main/java/de/littlerolf/crawlomat/TestRun.java
@@ -0,0 +1,17 @@
+package de.littlerolf.crawlomat;
+
+public class TestRun {
+
+	public TestRun() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

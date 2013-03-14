d991af3b6621bd7399b5fb27d08b4a3d7628b85f
Ole Wehrmeyer <olewehrmeyer@web.de>
2013-03-14 16:12:11 +0100

    Started impementing fuctionality
    Still needs work, see code

--- src/main/java/de/littlerolf/crawlomat/WebCrawler.java
+++ src/main/java/de/littlerolf/crawlomat/WebCrawler.java
@@ -1,30 +1,72 @@
 package de.littlerolf.crawlomat;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.xml.sax.SAXException;
+
+import com.meterware.httpunit.*;
+
+
 
 class WebCrawler implements Iterable<String> {
 
     private String url;
+    
+    private List<String> LinkList = new ArrayList<String>();
+    
 
     public WebCrawler(String url) {
         this.url = url;
     }
+    
+    public void crawl(int depth) throws MalformedURLException {
+    	WebLink[] list = getLinksFromSite(this.url);
+    	WebLink[] toCrawl;
+    	
+    	for (int i = depth;i>0;i--) { //For each depth one time
+    		//TODO: Implement depth-first search with depth as depth...
 
-    @Override
+    	}
+    }
+    
+
+    
+    public WebLink[] getLinksFromSite(String url) {
+    	try {
+    		
+        WebConversation conversation = new WebConversation();
+        WebRequest request = new GetMethodWebRequest(url);
+
+        WebResponse response = conversation.getResponse(request);
+        return response.getLinks();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (SAXException e) {
+			e.printStackTrace();
+			return null;
+		}
+        
+        
+    	
+    }
+
     public Iterator<String> iterator() {
         Iterator<String> it = new Iterator<String>() {
 
-            @Override
             public boolean hasNext() {
                 return false;
             }
 
-            @Override
             public String next() {
                 return null;
             }
 
-            @Override
             public void remove() {
 
             }

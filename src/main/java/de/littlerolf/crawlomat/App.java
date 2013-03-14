f89f613538cccb45c39802dbc087325a2adcf78f
Ole Wehrmeyer <olewehrmeyer@web.de>
2013-03-14 16:12:48 +0100

    Modified for testing WebCrawleer changes

--- src/main/java/de/littlerolf/crawlomat/App.java
+++ src/main/java/de/littlerolf/crawlomat/App.java
@@ -10,21 +10,28 @@ import java.io.IOException;
  */
 public class App {
     public static void main(String[] args) throws IOException, SAXException {
-        HttpUnitOptions.setScriptingEnabled(false);
+        System.out.println("Test");
+    	HttpUnitOptions.setScriptingEnabled(false);
         WebCrawler crawler = new WebCrawler("http://google.de");
 
+        //Test:
+        for (WebLink link :crawler.getLinksFromSite("www.google.de")) {
+        	System.out.println(link.getRequest().getURL().toString());
+        }
+        
+        
         for (String page : crawler) {
             System.out.println(page);
         }
 
         //Test:
-
+/*
         WebConversation conversation = new WebConversation();
 
         WebRequest request = new GetMethodWebRequest("http://google.de");
 
         WebResponse response = conversation.getResponse(request);
         for (WebLink link : response.getLinks())
-            System.out.println(link.getRequest().getURL().toString());
+            System.out.println(link.getRequest().getURL().toString());*/
     }
 }

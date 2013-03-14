83974bd5b031c35a0160cd0503c51038ec31ae94
Jan-Henrik Bruhn <jh.bruhn@me.com>
2013-03-14 11:16:03 +0100

    added rhino dependency, tested httpunit

--- src/main/java/de/littlerolf/crawlomat/App.java
+++ src/main/java/de/littlerolf/crawlomat/App.java
@@ -1,14 +1,30 @@
 package de.littlerolf.crawlomat;
 
+import com.meterware.httpunit.*;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SAXException {
+        HttpUnitOptions.setScriptingEnabled(false);
         WebCrawler crawler = new WebCrawler("http://google.de");
 
         for (String page : crawler) {
             System.out.println(page);
         }
+
+        //Test:
+
+        WebConversation conversation = new WebConversation();
+
+        WebRequest request = new GetMethodWebRequest("http://google.de");
+
+        WebResponse response = conversation.getResponse(request);
+        for (WebLink link : response.getLinks())
+            System.out.println(link.getRequest().getURL().toString());
     }
 }
